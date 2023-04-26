31f97aaeb11ab4034eb26fb635b47be65d97d3c7
João Pedro <joaopedromari28@gmail.com>
2023-04-26 10:28:26 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ItemComponent } from './item/item.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+ AppComponent,
+ ItemComponent
+ ],
+ imports: [
+ BrowserModule,
+ HttpClientModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
